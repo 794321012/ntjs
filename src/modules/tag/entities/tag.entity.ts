556e24f1af71929f774ea99a4370e055ec6fadbd
zhuchenhui <794321012@qq.com>
2024-01-06 03:45:36 +0800

    zushi

--- src/modules/tag/entities/tag.entity.ts
+++ src/modules/tag/entities/tag.entity.ts
@@ -1,6 +1,9 @@
 import { Common } from 'src/common/entity/common.entity';
+// 实体
 import { Article } from 'src/modules/article/entity/article.entity';
+// 装饰器
 import { Entity, Column, ManyToMany, JoinTable } from 'typeorm';
+// 校验器
 import { IsNotEmpty } from 'class-validator';
 @Entity()
 export class Tag extends Common {
